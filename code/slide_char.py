bbef6480d8fc6ebd380179d1e98c76dcbffe8d06
Thomas Moreau <toma.moral@gmail.com>
2013-12-10 00:48:38 +0100

    Sliding windows algorithm working

--- code/slide_char.py
+++ code/slide_char.py
@@ -1,32 +1,150 @@
 import numpy as np
 
-class sliding_window(object):
+import pyximport
+pyximport.install()
+from features import hog
+from scipy.misc import imresize, imread
+
+
+class SlidingWindow(object):
     '''
     Sliding windows algorithm
 
-    return a card of detection X(
+    return a card of detection X
     '''
-    def _init_(self, w, h, model):
+    def __init__(self, w, h):
         '''
         Parameters
         ----------
         (w,h): size of the window
-        model: model of detection
         '''
         self.width = w
         self.height = h
-        self.model = model
+        self.K = 61
+        self.model = []
+        for i in range(self.K):
+            self.model.append(svm.SVC(probability=True))
 
     def fit(self, X, y):
         '''
         Fit model to the observation (X,y) of detection
         '''
-        self.model.fit(X,y)
+        y = np.array(y)
+        X_feat = []
+        AR = [[] for i in range(self.K)]
+        for i, im in enumerate(X):
+            ai = im.size[0]/im.size[1]
+            AR[y[i]].append(ai)
+            im_r = im.resize((22,20))
+            X_feat.append(hog(im_r,4).reshape((-1,)))
 
-    def detection(self, th):
-        pass
+        for i in range(self.K):
+            y_feat = (y==i)
+            self.model[i].fit(X_feat, y_feat)
+        self.AR = np.array([[np.mean(AR[i]),np.var(AR[i])] for i in
+        range(self.K)])
 
-    def NMS(self, th):
-        pass
+    def detection(self, im, th, th1=0.1):
+        '''
+        Perform the sliding window detection on im
+        return a list with (x, y, class, proba)
 
+        '''
+        w = self.width
+        h = self.height
+        a = w/h
+        w0 = im.size[0] - w
+        h0 = im.size[1] - h
+        res = [[] for i in range(self.K)]
+        Ntot = w0*h0
+        
+        for i in range(w0):
+            for j in range(h0):
+                print '{:6.2%}'.format((i*h0+j)*1./Ntot)
+                X = im.crop((i,j,i+w,j+h))
+                X_r = X.resize((22,20))
+                X_feat = hog(X_r,4).reshape((1,-1))
+                p = []
+                for k in range(self.K):
+                    p.append(self.model[k].predict_proba(X_feat)[0,1])
+                cj = np.argmax(p)
+                muj,sigj = self.AR[cj]
+                GS = p[cj]*np.exp(-(muj-a)/(2*sigj))
+                if GS > th1:
+                    for k in range(self.K):
+                        res[k].append((i,j,p[k]))
+        res2 = []
+        for k in range(self.K):
+            res2.append(self.NMS(res[k], th))
+        return res2
+
+    def NMS(self, res, th):
+        '''
+        Fusion of the patch that overlap more than th
+        '''
+        w = self.width
+        h = self.height
+        r = []
+        while len(res)!=0:
+            i0 = np.argmax(res, axis=0)[2]
+            c = res[i0]
+            del res[i0]
+            l = [c[:2]]
+            i = 0
+            while i < len(res):
+                c2 = res[i]
+                intersec = (h-min(h,abs(c2[0]-c[0])))
+                intersec *= (w-min(w,abs(c2[1]-c[1])))
+                criterion = intersec / (2.*h*w - intersec)
+                if criterion > th:
+                    l.append(c2[:2])
+                    del res[i]
+                else:
+                    i+=1
+            r.append((np.mean(l,axis=0), c[2]))
+            
+        return r        
+
+
+if __name__== '__main__':
+    from sklearn import svm
+    from sklearn.preprocessing import LabelEncoder
+
+    import os
+    import Image
+    import joblib
+    test = SlidingWindow(30,24)
+    if not os.path.exists('../data/model/model.pickle'):
+
+        filenames = np.load('../data/char/list_char.npy')
+        labels = np.load('../data/char/lab_char.npy')
+        enc = LabelEncoder()
+        y = enc.fit_transform(labels)
+        X = []
+        for f in filenames:
+            im = Image.open(f)
+            X.append(im.resize([int(0.3 * s) for s in im.size]))
+            del im
+        test.fit(X,y)
+        joblib.dump(test.model, '../data/model/model.pickle')
+        joblib.dump(test.AR, '../data/model/AR.pickle')
+    else:
+        model = joblib.load('../data/model/model.pickle')
+        AR = joblib.load('../data/model/AR.pickle')
+        test.model = model
+        test.AR = AR
+    im = Image.open('../data/svt1/img/00_00.jpg')
+    im = im.resize((200,200))
+    res = test.detection(im,0.1)
+
+    from matplotlib.patches import Rectangle
+    import matplotlib.pyplot as plt
+
+    plt.clf()
+    ax = plt.gca()
+    for r in res[0]:
+        ax.add_patch(Rectangle(r[0],22,20))
+    ax.autoscale_view()
+    plt.show()
+    ax = plt.gca()
 
