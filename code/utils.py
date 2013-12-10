1e6158c6c45c00a32142df767a27185a1449e17d
Thomas Moreau <toma.moral@gmail.com>
2013-12-10 02:28:37 +0100

    Bug fix

--- code/utils.py
+++ code/utils.py
@@ -6,6 +6,6 @@ def display_char(L, c, w, h, im, col='blue'):
     plt.clf()
     ax = plt.gca()
     for rect in L[c]:
-        ax.add_patch(rectangle(rect[0], w, h, fc='none', ec=col))
+        ax.add_patch(Rectangle(rect[0], w, h, fc='none', ec=col))
     ax.autoscale_view()
     plt.imshow(im)
