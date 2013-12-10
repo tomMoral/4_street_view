0e6802d47c1b49cfad276738ad430063d3cf3a16
Thomas Moreau <toma.moral@gmail.com>
2013-12-10 13:36:24 +0100

    Add xml extractor utility

--- code/utils.py
+++ code/utils.py
@@ -10,3 +10,24 @@ def display_char(L, c, w, h, im, col='blue'):
     ax.autoscale_view()
     plt.imshow(im)
     plt.show()
+
+def extract_xml(fname, base_dir = '../data/'):
+    from os.path.join import join as jp
+    from xml.etree.ElementTree as ET
+    tree = ET.parse(fname)
+    root = tree.getroot()
+
+    filenames = []
+    labels = []
+
+    for child in root:
+        if child.get('tag') not in ['(',')','!','&','?',
+                                    '.','"',"'",'-',':',
+                                    u'\xa3', u'\xc9',
+                                    u'\xd1', u'\xe9']:
+            filenames.append(jp(base_dir, child.get('file')))
+            labels.append(child.get('tag'))
+
+    np.save('list_char', filenames)
+    np.save('lab_char', labels)
+
