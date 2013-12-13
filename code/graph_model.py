1bb07e487e7cb459b84d1db15a61f97e643cb5e5
Thomas Moreau <toma.moral@gmail.com>
2013-12-13 18:05:24 +0100

    Several changes

--- code/graph_model.py
+++ code/graph_model.py
@@ -22,7 +22,7 @@ class GraphicalModel(object):
         self.K = K
         self.prior = np.zeros((K+1,K+1))
 
-    def fit(self, mapc, w,h, th, enc, lambda0):
+    def fit(self, mapc, th, enc, lambda0):
         '''
         create a model for the decision fo the word
         '''
@@ -41,7 +41,7 @@ class GraphicalModel(object):
             for k in range(self.K):
                 Eu.append(1-p[k])
             Eu.append(mapc[i][2])
-            v.append([mapc[i][0], Eu])
+            v.append([mapc[i][0], Eu, mapc[i][3]])
             unary.append(Eu)
         self.vertices = v
 
@@ -60,10 +60,13 @@ class GraphicalModel(object):
             for j, v2 in enumerate(self.vertices[i+1:]):
                 dx = abs(v2[0][0]-v1[0][0])
                 dy = abs(v2[0][1]-v1[0][1])
+                w = v1[2][0]
+                h = v1[2][1]
                 if dx < th*w and dy < th*h:
-                    
-                    intersec = (h-min(h,abs(v2[0][0]-v1[0][0])))
-                    intersec *= (w-min(w,abs(v2[0][1]-v1[0][1])))
+                    intersec = (w-min(w,abs(v2[0][0]-v1[0][0])))
+                    intersec *= (h-min(h,abs(v2[0][1]-v1[0][1])))
+                    intersec *= 100./(w*h)
+
                     v0 = lambda0*np.exp(-(100-intersec)**2)
                     BinaryE = np.ones((K+1, K+1))*v0
                     BinaryE += self.prior
@@ -73,8 +76,8 @@ class GraphicalModel(object):
                     self.edges.append((i, j+1, intersec))
                     self.gm.addFactor(fid, [i, j+i+1])
 
-                elif dx > th*w:
-                    break
+                #elif dx > th*w:
+                #    break
         
         
 
@@ -99,11 +102,20 @@ class GraphicalModel(object):
         freq = np.zeros((self.K+1,self.K+1))
         n = 0.
         for w in vocabulary:
-            for i in range(len(w)-1):
-                c1 = enc.inverse_transform(w[i])
-                c2 = enc.invers_transform(w[i+1])
-                freq[c1][c2] += 1
-                n += 1.
+            if len(w) > 2:
+                for i in range(len(w)-1):
+                    if w[i] not in ['(',')','!','&','?',
+                                    '.','"',"'",'-',':',
+                                    u'\xa3', u'\xc9',',',
+                                    u'\xd1', u'\xe9','z']:
+                        if w[i+1] not in ['(',')','!','&','?',
+                                    '.','"',"'",'-',':',
+                                    u'\xa3', u'\xc9',',',
+                                    u'\xd1', u'\xe9','z']:
+                            c1 = enc.transform(w[i])
+                            c2 = enc.transform(w[i+1])
+                            freq[c1][c2] += 1
+                            n += 1.
         for i in range(self.K):
             for j in range(self.K):
                 self.prior[i][j] = lambda_l*(1-freq[i][j]/n)
@@ -114,9 +126,9 @@ class GraphicalModel(object):
         print 'TODO: Node specific prior' 
 
 
-    def predict(self):
+    def predict(self, step=100):
         import opengm
-        parameter = opengm.InfParam(steps=100)
+        parameter = opengm.InfParam(steps=step)
         algo = opengm.inference.TrwsExternal(self.gm, parameter=parameter)
         algo.infer()
         return algo.arg()
